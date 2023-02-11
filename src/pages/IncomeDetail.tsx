002fb2134558fce86e12cf7553ebbc80fd59c127
FatihG34 <fthgnydn34@gmail.com>
2023-02-11 23:16:31 +0300

    new page added

---
+++ src/pages/IncomeDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from '../components/Cards'
+
+const IncomeDetail = () => {
+  return (
+    <div>
+      <Cards/>
+    </div>
+  )
+}
+
+export default IncomeDetail
